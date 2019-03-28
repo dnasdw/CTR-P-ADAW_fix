196c52ea9dfef15ae93bf1f3002eb1de7e672235
Sun Daowen <sundaowen@gmail.com>
2019-03-29 00:17:36 +0800

    init CTR-P-ADAW

---
+++ CTR-P-ADAW/0-0_reset_zh-Hans.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Nintendogs + Cats - Shiba & New Friends (TWN) (zh-Hans).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hans_ADAWTOOLS.exe
+POPD
---
+++ CTR-P-ADAW/0-1_reset_zh-Hant.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Nintendogs + Cats - Shiba & New Friends (TWN) (zh-Hant).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hant_ADAWTOOLS.exe
+POPD
